57d74fa2ba7bc1537dd2cc638342fa474b0c334b
Kurulucifer <28943130+Kurulucifer@users.noreply.github.com>
2026-02-25 21:23:06 -0800

    Create base index.html and javascript

---
+++ javascript.js
@@ -0,0 +1,36 @@
+let one = 0;
+let two = 0;
+let oper = ""
+
+function add(x, y) {
+    return x + y;
+}
+
+function subtract(x, y) {
+    return x - y;
+}
+
+function multiply(x, y) {
+    return x * y;
+}
+
+function divide(x, y) {
+    return x / y;
+}
+
+function evaluate(x, y, op) {
+    switch (op) {
+        case "+":
+            return add(x, y);
+        case "-":
+            return subtract(x, y);
+        case "*":
+            return multiply(x, y);
+        case "/":
+            return divide(x, y);
+    }
+}
+
+/*
+What is the gameplan here
+*/
